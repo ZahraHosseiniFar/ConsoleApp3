ede2d3a0fcc2c7b761d92c5747c7efdeffd967c1
Ali Balaei <ZAHRA.HOSSEINI.FAR2000@gmail.com>
2022-11-03 01:58:04 +0330

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            bool Content = true;
+            bool NoContent = false;
+            Console.WriteLine("test is {0}", Content);
+            Console.WriteLine("test is {0}", NoContent);
+            Console.ReadKey();
+        }
+    }
+}
